8e06ce3e2842552c4c4d3a991757dcbe541f6fde
sa1to21 <102365702+sa1to21@users.noreply.github.com>
2025-08-24 15:55:45 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+js
+import { initStorage } from './storage.js';
+import { updateAllBalances } from './transactions.js';
+import { initUI } from './ui.js';
+
+
+async function init() {
+await initStorage();
+updateAllBalances();
+initUI();
+console.log("Приложение инициализировано");
+}
+
+
+init();
