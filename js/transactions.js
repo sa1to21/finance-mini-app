e6d05ceb401a44426602884b3c91bc6e1769f258
sa1to21 <102365702+sa1to21@users.noreply.github.com>
2025-08-24 16:58:25 +0400

    Delete js/transactions.js

--- js/transactions.js
+++
@@ -1,9 +0,0 @@
-import { accounts, transactions, saveData } from './storage.js';
-import { formatCurrency } from './utils.js';
-
-
-export function addTransaction() { /* логика из index.html */ }
-export function deleteTransaction(id) { /* логика удаления */ }
-export function applyFilters() { /* фильтры */ }
-export function updateFilters() { /* обновление фильтров */ }
-export function updateAllBalances() { /* общий баланс + краткая сводка */ }
