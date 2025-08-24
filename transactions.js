bf5a520a08b3e7f9ad4c54d78f45db70175e256d
sa1to21 <102365702+sa1to21@users.noreply.github.com>
2025-08-24 15:57:49 +0400

    Create transactions.js

---
+++ transactions.js
@@ -0,0 +1,10 @@
+js
+import { accounts, transactions, saveData } from './storage.js';
+import { formatCurrency } from './utils.js';
+
+
+export function addTransaction() { /* логика из index.html */ }
+export function deleteTransaction(id) { /* логика удаления */ }
+export function applyFilters() { /* фильтры */ }
+export function updateFilters() { /* обновление фильтров */ }
+export function updateAllBalances() { /* общий баланс + краткая сводка */ }
