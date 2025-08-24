34a2c2e7da242b199e7940bc21287f6f4125d295
sa1to21 <102365702+sa1to21@users.noreply.github.com>
2025-08-24 15:58:06 +0400

    Create accounts.js

---
+++ accounts.js
@@ -0,0 +1,9 @@
+js
+import { accounts, transactions, saveData } from './storage.js';
+
+
+export function addAccount() { /* логика добавления счёта */ }
+export function deleteAccount(id) { /* логика удаления счёта */ }
+export function displayAccounts() { /* вывод карточек */ }
+export function updateAccountSelect() { /* селект счетов */ }
+export function updateAccountSelects() { /* from/to счета */ }
