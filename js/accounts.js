04180769695135bddd05b4a7b7d55a28398dac1a
sa1to21 <102365702+sa1to21@users.noreply.github.com>
2025-08-24 16:57:58 +0400

    Delete js/accounts.js

--- js/accounts.js
+++
@@ -1,8 +0,0 @@
-import { accounts, transactions, saveData } from './storage.js';
-
-
-export function addAccount() { /* логика добавления счёта */ }
-export function deleteAccount(id) { /* логика удаления счёта */ }
-export function displayAccounts() { /* вывод карточек */ }
-export function updateAccountSelect() { /* селект счетов */ }
-export function updateAccountSelects() { /* from/to счета */ }
